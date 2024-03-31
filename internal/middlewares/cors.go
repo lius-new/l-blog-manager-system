ad9eb46241dcc3860a030c0f1a20680b71e5a3f9
lius-new <liushui_new@126.com>
2024-03-31 21:47:53 +0800

    fix: cors allow origin link

--- internal/middlewares/cors.go
+++ internal/middlewares/cors.go
@@ -4,8 +4,23 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+var ALLOO_RIGINS = []string{
+	"http://localhost:5173",
+	"http://localhost:3000",
+	"http://101.42.173.48:8080",
+}
+
 func CorsMiddlware(c *fiber.Ctx) error {
-	c.Set("Access-Control-Allow-Origin", "http://localhost:5173")
+
+	origin := c.Get("Origin")
+
+	for _, v := range ALLOO_RIGINS {
+		if v == origin {
+			c.Set("Access-Control-Allow-Origin", v)
+			break
+		}
+	}
+
 	c.Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
 	c.Set("Access-Control-Allow-Headers", "Content-Type, Cookie")
 	c.Set("Access-Control-Allow-Credentials", "true")
