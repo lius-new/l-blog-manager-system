00eacfa83a9a4d2ad15c5d8dbe201a3c6a2fbfb1
lius-new <liushui_new@126.com>
2024-04-06 20:09:41 +0800

    feat: 批量图片上传

--- internal/routers/file.go
+++ internal/routers/file.go
@@ -1,6 +1,8 @@
 package routers
 
 import (
+	"fmt"
+	"mime/multipart"
 	"os"
 
 	"github.com/gofiber/fiber/v2"
@@ -10,6 +12,7 @@ import (
 func RegisterFileHanlder(app *fiber.App) {
 	api := app.Group("/api/file")
 	api.Get("/:hash", viewFileHandle)
+	api.Post("/upload-images", uploadImageFileHandle)
 }
 
 func viewFileHandle(ctx *fiber.Ctx) error {
@@ -27,4 +30,43 @@ func viewFileHandle(ctx *fiber.Ctx) error {
 	}
 
 	return ctx.SendFile(os.Getenv("COVER_PATH") + filename)
+}
+
+// uploadImageFileHandle: 保存图片
+func uploadImageFileHandle(ctx *fiber.Ctx) error {
+	var (
+		form *multipart.Form
+		err  error
+	)
+
+	if form, err = ctx.MultipartForm(); err != nil {
+		return err
+	}
+	files := form.File["images"]
+
+	hashs := make([]string, 0)
+	for _, v := range files {
+		file, err := v.Open()
+		if err != nil {
+			return err
+		}
+		defer file.Close()
+		hash, err := utils.Hash(file)
+		if err != nil {
+			return err
+		}
+		hashs = append(hashs, hash)
+	}
+
+	for i, file := range files {
+		// save to example : COVER_PATH/文件hash.后缀 =>./data/covers/hsdjf24hjsfh283sf.png
+		savePath := fmt.Sprintf("%s/%s.%s", os.Getenv("COVER_PATH"), hashs[i], utils.GetFileSuffix(file.Filename))
+		if exist, _ := utils.FileExist(savePath, os.Getenv("COVER_PATH")); len(exist) == 0 { // 不存在就保存
+			if err := ctx.SaveFile(file, savePath); err != nil {
+				return err
+			}
+		}
+	}
+
+	return ctx.JSON(fiber.Map{"status": true, "data": hashs})
 }
